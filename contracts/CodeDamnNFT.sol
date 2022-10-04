46298b62983d685c08d6ee7f3a3b26182d8aa493
navyansh007 <91735202+navyansh007@users.noreply.github.com>
2022-10-04 22:25:07 +0530

    Update CodeDamnNFT.sol

--- contracts/CodeDamnNFT.sol
+++ contracts/CodeDamnNFT.sol
@@ -10,8 +10,6 @@ contract CodeDamnNFT is ERC721, ERC721URIStorage, Ownable {
 
     Counters.Counter private _tokenIdCounter;
 
-    mapping(string => uint8) existingURIs;
-
     constructor() ERC721("CodeDamnNFT", "CDNFT") {}
 
     function _baseURI() internal pure override returns (string memory) {
@@ -38,30 +36,5 @@ contract CodeDamnNFT is ERC721, ERC721URIStorage, Ownable {
         returns (string memory)
     {
         return super.tokenURI(tokenId);
-    }
-
-    function isContentOwned(string memory uri) public view returns (bool){
-        return existingURIs[uri] == 1;
-    }
-
-    function payToMint(address recipient, string memory metadataURI) public payable returns (uint256){
-        require(existingURIs[metadataURI] != 1, "NFT is already minted");
-        require(msg.value >= 0.01 ether, "Price is too low");
-
-        uint256 newItemId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        existingURIs[metadataURI] = 1;
-
-        _mint(recipient, newItemId);
-        _setTokenURI(newItemId, metadataURI);
-        return newItemId;
-    }
-
-    function count() public view returns (uint256){
-        return _tokenIdCounter.current();
-    }
-
-    function getCurrentContractBalance() public view returns (uint256){
-        return address(this).balance;
     }
 }
